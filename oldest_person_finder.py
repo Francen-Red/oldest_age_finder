7beebdf54553505dd2255268e5bafba1445ea0ec
Francen-Red <francenvenissered@gmail.com>
2024-10-22 06:40:54 +0800

    Updated the retry choice for name and added retry choice for age, both with valid input conditions

--- oldest_person_finder.py
+++ oldest_person_finder.py
@@ -28,12 +28,12 @@ while True:
         elif confirm != "yes": 
             retry_choice = input("Retry name, age, or both? (name/age/both): ") # If not, ask the user if they want to retry name, age, or both
             
-        while True:
             if retry_choice == "name":
-                input_name = input("Please input a new name: ")
-                if any(ord(char) in invalid_characters for char in input_name):  # Check if the ASCII values of the name include invalid characters using ord() function
-                    print("Please input a valid name.")  # If yes, ask the user again for a valid name
-                continue 
+                while True:
+                    input_name = input("Please input a new name: ")
+                    if any(ord(char) in invalid_characters for char in input_name):  # Check if the ASCII values of the name include invalid characters using ord() function
+                        print("Please input a valid name.")  # If yes, ask the user again for a valid name
+                    break 
 
             elif retry_choice == "age":
                 while True:
@@ -42,8 +42,7 @@ while True:
                         break
                     except:
                         print("Please input a valid number for age.")
-
-                break
+                
 
             
     break  # Move on if the name is valid
