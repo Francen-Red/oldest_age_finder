a347021a910ef91088025cbf8e60fba6a835a6aa
Francen-Red <francenvenissered@gmail.com>
2024-10-21 23:29:19 +0800

    Pseudocode for the program

---
+++ oldest_person_finder.py
@@ -0,0 +1,14 @@
+# Create a dictionary to store user's input
+# Create a loop for the name
+    # Create a loop for the age, inside the first loop
+    # Create a loop for the retry confirmation
+    # Ask if the user wants to input another entry using a variable, if not, break the loop
+# Create oldest_name and oldest_age variables, store the first input of the user (index 0)
+# Compare the age in oldest_age to the current age (new entry)
+    # If the new information/input is greater than the age in oldest_age variable, update oldest_age and oldest_name
+    #Print the oldest person using a function that will print the oldest_name and oldest_age
+
+        
+
+
+
