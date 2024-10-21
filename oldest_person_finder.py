f86bd92fef30de6cf9b603c55631ea713dee54c0
Francen-Red <francenvenissered@gmail.com>
2024-10-21 23:58:38 +0800

    Created a loop for confirmation that will ask the user to retry name, age, or both if the input is incorrect

--- oldest_person_finder.py
+++ oldest_person_finder.py
@@ -14,9 +14,15 @@ while True:
             print("Please input a valid number for age.") # If age is not valid, print an error message
 
     # Create a loop for the retry confirmation
+    while True:
+        confirm = input(f"Name = {input_name}, Age = {input_age}. Is this correct? (yes or no): ") # Use a function to display the user's input
+        if confirm == "yes":
+            break  # Move on if user confirms that the input is correct
+        elif confirm != "yes": 
+            retry_choice = input("Retry name, age, or both? (name/age/both): ") # If not, ask the user if they want to retry name, age, or both
 
     # Ask if the user wants to input another entry using a variable, if not, break the loop
 # Create oldest_name and oldest_age variables, store the first input of the user (index 0)
 # Compare the age in oldest_age to the current age (new entry)
     # If the new information/input is greater than the age in oldest_age variable, update oldest_age and oldest_name
-    #Print the oldest person using a function that will print the oldest_name and oldest_age
+    # Print the oldest person using a function that will print the oldest_name and oldest_age
