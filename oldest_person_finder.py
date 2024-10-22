f80d9fbdb2101a5460d7f407cb828bcd2fda6e09
Francen-Red <francenvenissered@gmail.com>
2024-10-22 22:35:05 +0800

    Added main loop and enable user to input another entry

--- oldest_person_finder.py
+++ oldest_person_finder.py
@@ -4,15 +4,20 @@ user_info = {}
 # Create a list of invalid characters for the name
 invalid_characters = [63, 123, 125, 91, 93, 36, 42, 37, 33, 38, 64, 94, 124, 92, 47, 60, 62] # Use ASCII values to avoid confusion or error
 
-# Create a loop for the name
+# Main loop to repeatedly ask for user inputs until they decide to stop
 while True:
-    input_name = input("Please input a name: ")
 
-    if any(ord(char) in invalid_characters for char in input_name):  # Check if the ASCII values of the name include invalid characters using ord() function
-        print("Please input a valid name.")  # If yes, ask the user again for a valid name
-        continue 
+# Create a loop for the name
+    while True:
+        input_name = input("Please input a name: ")
 
-    # Create a loop for the age, inside the first loop
+        if any(ord(char) in invalid_characters for char in input_name):  # Check if the ASCII values of the name include invalid characters using ord() function
+            print("Please input a valid name.")  # If yes, ask the user again for a valid name
+            continue 
+        else:
+            break  # Move on if the name is valid
+
+# Create a loop for the age
     while True:
         try:
             input_age = int(input("Please input age: "))
@@ -20,7 +25,7 @@ while True:
         except:
             print("Please input a valid number for age.") # If age is not valid, print an error message
 
-    # Create a loop for the retry confirmation   
+# Create a loop for the retry confirmation   
     while True:
         confirm = input(f"Name = {input_name}, Age = {input_age}. Is this correct? (yes or no): ") # Use a function to display the user's input
         if confirm == "yes":
@@ -39,26 +44,28 @@ while True:
                 while True:
                     try:
                         input_age = int(input("Please input a new age: "))
-                        break
+                        break   # If age is valid, break out of this loop
                     except:
-                        print("Please input a valid number for age.")
+                        print("Please input a valid number for age.") # If age is not valid, print an error message
 
             elif retry_choice == "both":
                 while True:
                     input_name = input("Please input a new name: ")
-                    if any(ord(char) in invalid_characters for char in input_name):  # Check if the ASCII values of the name include invalid characters using ord() function
+                    if any(ord(char) in invalid_characters for char in input_name): 
                         print("Please input a valid name.")  # If yes, ask the user again for a valid name
                     break 
                 while True:
                     try:
                         input_age = int(input("Please input a new age: "))
-                        break
+                        break   # If age is valid, break out of this loop
                     except:
-                        print("Please input a valid number for age.")
+                        print("Please input a valid number for age.")  # If age is not valid, print an error message
             
-    break  # Move on if the name is valid
+# Ask if the user wants to input another entry using a variable
+    another_entry = input("Do you want to input another entry? (yes/no): ")
+    if another_entry != "yes":
+        break  # If not, break the loop
     
-    # Ask if the user wants to input another entry using a variable, if not, break the loop
 # Create oldest_name and oldest_age variables, store the first input of the user (index 0)
 # Compare the age in oldest_age to the current age (new entry)
     # If the new information/input is greater than the age in oldest_age variable, update oldest_age and oldest_name
